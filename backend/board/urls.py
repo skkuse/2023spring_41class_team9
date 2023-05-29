5bc4d399826027fd4f2b11b0ab20c3cce47862ed
Minseong Kim <50157859+minseong0106@users.noreply.github.com>
2023-05-30 04:37:30 +0900

    <fix> add real mode function

--- backend/board/urls.py
+++ backend/board/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('practice/', views.practice),
     path('real/', views.real),
     path('practice/practice_mode_start/<int:id>', views.practice_start, name='problem-view'),
+    path('real/real_mode_start/<int:id>', views.real_start, name='problem-view2'),
 ]
--- backend/board/views.py
+++ backend/board/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Problems
+import random
 
 # Create your views here.
 
@@ -7,10 +8,12 @@ def practice(request):
     return render(request, 'practice.html')
 
 def real(request):
-    return render(request, 'real.html')
+    return render(request, 'real.html', {'random_number':random.randint(11,15)})
 
 def practice_start(request, id):
     problem = Problems.objects.get(problem_id=id)
-    print(problem.problem)
     return render(request, 'practice_mode_start.html',{'problem_content':problem.problem, 'problem_title':problem.problem_title, 'io_example1':problem.test_1, 'io_ex_answer1':problem.test_ans_1})  
 
+def real_start(request, id):
+    problem = Problems.objects.get(problem_id=id)
+    return render(request, 'real_mode_start.html',{'problem_content':problem.problem, 'problem_title':problem.problem_title, 'io_example1':problem.test_1, 'io_ex_answer1':problem.test_ans_1})  
