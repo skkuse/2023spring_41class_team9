754c4e89bbc54e489553124fe34b7772a7182c52
Minseong Kim <50157859+minseong0106@users.noreply.github.com>
2023-05-30 22:09:07 +0900

    <fix> html file

--- backend/board/views.py
+++ backend/board/views.py
@@ -15,3 +15,6 @@ def practice_start(request, id):
 def real_start(request, id):
     problem = Problems.objects.get(problem_id=id)
     return render(request, 'real_mode_start.html',{'problem_title':problem.problem_title, 'problem_content':problem.problem, 'problem_input':problem.problem_input, 'problem_output':problem.problem_output, 'io_example1':problem.test_1, 'io_ex_answer1':problem.test_ans_1, 'io_example2':problem.test_2, 'io_ex_answer2':problem.test_ans_2, 'io_example3':problem.test_3, 'io_ex_answer3':problem.test_ans_3})
+
+def review(request):
+    return render(request, 'review.html')
