291c358ba6b2773947781d533658d5e2b12cab05
Park-Jubong <bong6642@gmail.com>
2023-05-31 15:06:41 +0900

    <feat> Add GPT hint

--- backend/board/views.py
+++ backend/board/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Problems
 import random
-import builtins
-import io
-import types
 import subprocess
 import os
+import requests
 
 def practice(request):
     return render(request, 'practice.html')
@@ -13,11 +12,10 @@ def practice(request):
 def real(request):
     return render(request, 'real.html', {'random_number':random.randint(11,15)})
 
-def practice_start(request, id):
+def practice_start(request, id, hint_id=1):
     problem = Problems.objects.get(problem_id=id)
 
-    if request.method == 'POST':
-        
+    if request.method == 'POST':      
         user_answer = request.POST.get('user_answer', '')  # 폼 요소의 name 속성 값을 사용하여 데이터 불러오기
         with open('a.py', 'w') as file:
             file.write(user_answer)
@@ -54,9 +52,47 @@ def practice_start(request, id):
                 print("Wrong!!!!")
 
 
-        
-        
+        user_code = user_answer
+        print(user_code)
+        problem_content = problem.problem
+        problem_input = problem.problem_input
+        problem_output = problem.problem_output
+        prompt = f"문제는 다음과 같다.\n{problem_content}\n문제의 입력에 관한 설명은 다음과 같다.\n{problem_input}\n문제의 출력에 관한 설명은 다음과 같다.\n{problem_output}"
+        prompt_hint_1 = "해당 문제를 해결하기 위한 간단한 힌트를 줘."
+        prompt_hint_2 = f"현재까지 작성한 code는 다음과 같다.\n{user_code}\n이 문제를 해결하기 위한 다음단계에 관한 간단한 힌트를 줘."
+        prompt_hint_3 = f"현재까지 작성한 code는 다음과 같다.\n{user_code}\n이 코드를 보고 틀린 부분에 대한 간단한 힌트를 줘."
+        prompt_hint_4 = "해당 문제를 보고 python을 사용해 skeleton code를 짜줘."
+
+        if hint_id == 1:
+            content = prompt + prompt_hint_1
+        elif hint_id == 2:
+            content = prompt + prompt_hint_2
+        elif hint_id == 3:
+            content = prompt + prompt_hint_3
+        elif hint_id == 4:
+            content = prompt + prompt_hint_4
+        url = 'https://api.openai.com/v1/chat/completions'
+        payload = {
+            'messages': [
+            {'role': 'user', 'content': content}
+        ],
+        'model': 'gpt-3.5-turbo',
+        'max_tokens': 1000
+        }
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer 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'
+        }
+        response = requests.post(url, json=payload, headers=headers)
+        if response.status_code == 200:
+            hint = response.json()['choices'][0]['message']['content']
+            return render(request, 'practice_mode_start.html',{'problem_title':problem.problem_title, 'problem_content':problem.problem, 'problem_input':problem.problem_input, 'problem_output':problem.problem_output, 'io_example1':problem.test_1, 'io_ex_answer1':problem.test_ans_1, 'io_example2':problem.test_2, 'io_ex_answer2':problem.test_ans_2, 'io_example3':problem.test_3, 'io_ex_answer3':problem.test_ans_3, 'hint': hint})
+        else:
+            return HttpResponse('An error occurred', status=response.status_code)
+    
     return render(request, 'practice_mode_start.html',{'problem_title':problem.problem_title, 'problem_content':problem.problem, 'problem_input':problem.problem_input, 'problem_output':problem.problem_output, 'io_example1':problem.test_1, 'io_ex_answer1':problem.test_ans_1, 'io_example2':problem.test_2, 'io_ex_answer2':problem.test_ans_2, 'io_example3':problem.test_3, 'io_ex_answer3':problem.test_ans_3})
+
 
 def real_start(request, id):
     problem = Problems.objects.get(problem_id=id)
@@ -99,6 +135,7 @@ def real_start(request, id):
                 print("Wrong!!!!")
 
     return render(request, 'real_mode_start.html',{'problem_title':problem.problem_title, 'problem_content':problem.problem, 'problem_input':problem.problem_input, 'problem_output':problem.problem_output, 'io_example1':problem.test_1, 'io_ex_answer1':problem.test_ans_1, 'io_example2':problem.test_2, 'io_ex_answer2':problem.test_ans_2, 'io_example3':problem.test_3, 'io_ex_answer3':problem.test_ans_3})
+        
 
 def review(request):
     return render(request, 'review.html')
