af1725b9281c721e45b30c71a8a2d949e00498f5
HyeonjinChoi <eaglushyeonjin@gmail.com>
2023-06-11 10:58:24 +0900

    <feat> Add user problems DB table

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from board.models import Problems
 
 class User(models.Model):
     username = models.CharField(max_length=64,
@@ -8,8 +9,8 @@ class User(models.Model):
     registered_dttm = models.DateTimeField(auto_now_add=True,
                                            verbose_name='등록시간')
 
-    # score = models.IntegerField(default=0)
-    # stage = models.IntegerField(default=1)
+    score = models.IntegerField(default=0)
+    stage = models.IntegerField(default=1)
     
     def __str__(self):
         return self.username
@@ -18,3 +19,13 @@ class User(models.Model):
         db_table = 'practice_user'
         verbose_name = '사용자'
         verbose_name_plural = '사용자'
+
+class UserProblem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    problem = models.ForeignKey(Problems, on_delete=models.CASCADE)
+    
+    hint = models.IntegerField(default=0)
+    solved = models.IntegerField(default=0)
+    started = models.IntegerField(default=0)
+    class Meta:
+        db_table = 'user_problems'
